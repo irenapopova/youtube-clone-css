0017f4337cd5f8f319dc407f5a0bb7b2dc2b2e9c
Irena <irenejpopova6@gmail.com>
2020-05-12 13:18:58 +0200

    small style changes

--- js/main.js
+++ js/main.js
@@ -12,22 +12,42 @@ inner_nav.innerHTML = `<ul id="menu" class="menu active">
 
 
 // Hamburger menu toggle //
-const contenedor_video = document.getElementById("main_container");
-const contenedor_menu = document.getElementById("menu");
 
-document.getElementById("boton-menu").addEventListener("click", () => {
-  contenedor_video.classList.toggle("active");
-  contenedor_menu.classList.toggle("active");
+
+
+
+
+//pop-menu - when click on avatar icon//
+
+const pop_menu = document.getElementById("pop_menu");
+
+document.getElementById("user").addEventListener("click", () => {
+  pop_menu.classList.toggle("able");
 });
-//min width 800px//
-const resize = () => {
-  if (window.innerWidth >= 800) {
-    contenedor_video.classList.remove("active");
-  } else {
-    contenedor_video.classList.add("active");
-  }
-};
-resize();
-window.addEventListener("resize", () => {
-  resize();
+
+document.getElementById("right").addEventListener("click", () => {
+  pop_menu.classList.add("able");
+});
+
+document.getElementById("left").addEventListener("click", () => {
+  pop_menu.classList.add("able");
+});
+document.getElementById("menu").addEventListener("click", () => {
+  pop_menu.classList.add("able");
+});
+
+//functions to change the background----- dark mode simulation/////////
+
+const btnSwitch = document.querySelector('#switch');
+const right = document.querySelector('#right');
+const left = document.querySelector('#left');
+const nav = document.querySelector('#nav');
+const menu = document.querySelector('#menu');
+
+btnSwitch.addEventListener('click', () => {
+  btnSwitch.classList.toggle('active');
+  right.classList.toggle('d_mode')
+  left.classList.toggle('d_mode')
+  nav.classList.toggle('d_mode')
+  menu.classList.toggle('d_mode')
 });
